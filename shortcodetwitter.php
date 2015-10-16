78956fafd9bb73891c24c8ede8ed216dfc5f61fc
Pedro Pinto <pedropintomena@gmail.com>
2015-10-16 09:56:40 +0200

    added css file and equeue dashicon and css file

--- shortcodetwitter.php
+++ shortcodetwitter.php
@@ -25,15 +25,25 @@ function shortcodetwitter_sharetwitter( $atts, $content = null ) {
         'size' => 'small',
     ), $atts );
 
-
-
-	$url_post = urlencode( get_permalink() );
-	$title_post = urlencode( get_the_title() );
-	$link_twitter = 'http://twitter.com/intent/tweet?url=' . $url_post . '&amp;text=' . $title_post; 
-	$class = '';
+	$link_twitter = 'http://twitter.com/intent/tweet?url=' . urlencode( get_permalink() ) . '&amp;text=' . urlencode( get_the_title() );
 	
-	$output = '<a class="' . $class . '" target="_blank" href="' . $link_twitter . '" title="Compartir en Twitter">' . $atts_sharetwitter['text'] . '</a>';
+	$bigclass = '';
+	if ( $atts_sharetwitter['size'] == 'big' )
+		$bigclass = 'sct-twitter-big';
+	
+	$output = '<a class="sct-twitter ' . $bigclass . '" target="_blank" href="' . $link_twitter . '" title="Compartir en Twitter">' . $atts_sharetwitter['text'] . '</a>';
 	
 	return $output;
 }
 add_shortcode( 'sct_sharetwitter', 'shortcodetwitter_sharetwitter' );
+
+
+/*
+ *  Enqueue css, javascript, and fonts files needed
+ */
+function shortcodetwitter_css() {
+
+	wp_enqueue_style( 'shortcodetwitter-style', plugins_url( '/shortcodetwitter.css', __FILE__ ) );
+    wp_enqueue_style( 'dashicons' );
+}
+add_action( 'wp_enqueue_scripts', 'shortcodetwitter_css' );
